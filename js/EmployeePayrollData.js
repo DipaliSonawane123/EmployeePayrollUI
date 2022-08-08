5a69976bdc875ce00f774f1081e4505f7b386c5d
Dipali <dipssonawane123@gmail.com>
2022-08-09 01:39:45 +0530

    Modified:Employee Payroll data add in java script is modified

--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -13,8 +13,10 @@ class EmployeePayrollData {
         return this._name;
     }
     set name(name) {
-       
+        let nameRegex = RegExp('^[A-z]{1}[a-z]{2,}$');
+        if (nameRegex.test(name))
             this._name = name;
+            else throw 'Name is incorrect';
     }
     get profilePic() {
         return this._profilePic;
@@ -50,7 +52,25 @@ class EmployeePayrollData {
         return this._startDate;
     }
     set startDate(startDate){
-        
+        let currentDate=new Date();
+        if (startDate>currentDate){
+            throw "start date is a future date";
+        }
+        var diff=Math.abs(currentDate.getTime-startDate.getTime());
+        if(diff/(1000*60*60*24)>30){
+            throw 'start date is beyond 30 days'
+        }
         this._startDate=startDate;
+    }
+     /**
+     * method
+     */
+      toString(){
+        const options={year:'numeric',month:'long',day:'numeric'};
+        const empDate=!this._startDate ? "undefined" :
+        this.startDate.toLocalDateString("en-US",options);
+        return "id=" + this.id + ",name='" +this.name + ", gender='" + this.gender +
+        ",profilePic='" + this.profilePic + ", department=" + this.department +
+        ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
     }
 }
